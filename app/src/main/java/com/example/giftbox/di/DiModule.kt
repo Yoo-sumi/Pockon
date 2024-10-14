e4abf810ed5e33d7cd96da226adcdaf5eddd9746
yoosumi <hugbee@naver.com>
2024-10-14 11:04:03 +0900

    feat: firestorage 제거

--- app/src/main/java/com/example/giftbox/di/DiModule.kt
+++ app/src/main/java/com/example/giftbox/di/DiModule.kt
@@ -11,8 +11,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -33,12 +31,6 @@ class DiModule {
     @Provides
     fun provideFirebaseFirestore() : FirebaseFirestore {
         return FirebaseFirestore.getInstance()
-    }
-
-    @Singleton
-    @Provides
-    fun provideFirebaseStorage() : FirebaseStorage {
-        return Firebase.storage
     }
 
     @Singleton
