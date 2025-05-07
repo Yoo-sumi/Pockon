6a05696e4a1b2c6e654c873398b5d0815fd1459d
yoosumi <hugbee@naver.com>
2025-05-07 17:58:34 +0900

    chore: 버전업 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.sumi.pockon"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.1"
+        versionCode = 3
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
